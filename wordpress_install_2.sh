9a99f841bcb8f8e0c8c000360e332c4daca74c19
RomanNum3ral <116513200+RomanNum3ral@users.noreply.github.com>
2023-08-31 14:12:04 -0400

    Update wordpress_install_2.sh

--- wordpress_install_2.sh
+++ wordpress_install_2.sh
@@ -4,7 +4,6 @@ sudo apt install php wget unzip -y
 sudo wget https://wordpress.org/latest.zip
 sudo unzip latest.zip
 cd wordpress
-sudo mkdir /var/www/html
 sudo cp -r * /var/www/html
 cd /var/www/html
 sudo rm -rf index.html
