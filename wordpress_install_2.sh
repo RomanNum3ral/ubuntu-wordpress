5d358b9814b30939b762ec6e5b5491181a9e6976
RomanNum3ral <116513200+RomanNum3ral@users.noreply.github.com>
2023-08-31 14:30:04 -0400

    Update wordpress_install_2.sh

--- wordpress_install_2.sh
+++ wordpress_install_2.sh
@@ -7,3 +7,7 @@ cd wordpress
 sudo cp -r * /var/www/html
 cd /var/www/html
 sudo rm -rf index.html
+sudo apt install php-mysql php-cgi php-cli php-gd -y
+sudo systemctl restart apache2
+sudo chown -R www-data:www-data /var/www/
+sudo mysql -u root -p
